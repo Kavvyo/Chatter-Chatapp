46e181c33db5b0e233b4561f7c2dab9264097cbe
Kavvyo <kavindud73@gmail.com>
2025-06-13 09:41:20 +0530

    Final Project

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ import path from 'path';
 import { connectDB } from './lib/db.js';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
-import friendsRoute from './routes/friends.route.js'
+import friendsRoute from './routes/friends.route.js';
 import { app, server } from './lib/socket.js';
 
 
@@ -20,7 +20,7 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieparser());
 app.use(cors({
-    origin: ["http://localhost:5173", "http://192.168.8.194:5173"],
+    origin: "http://localhost:5173",
     credentials: true
 }));
 
